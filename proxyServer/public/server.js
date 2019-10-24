25dc7528c033227ee778e7c9e039056c08e89747
tarora2014 <tarora@g.clemson.edu>
2019-10-24 10:43:14 -0700

    proxy added

---
+++ proxyServer/public/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const PORT = 4000;
+
+
+//,amage CORS policy
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use(morgan('dev'));
+app.use("/:productId/checkout",express.static(path.join(__dirname, 'lib')));
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
