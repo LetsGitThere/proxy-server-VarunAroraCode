b8e08a3aedd6f48a1537a4a404324d80e5bddcae
tarora2014 <tarora@g.clemson.edu>
2019-10-24 19:05:33 -0700

    working proxy

--- proxyServer/public/server.js
+++ proxyServer/public/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const PORT = 4000;
+const morgan = require('morgan')
+const PORT = 4001;
 
-
-//,amage CORS policy
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+app.use(morgan('dev'))
+app.use("/:productID",express.static(path.join(__dirname, 'lib')));
+app.get("*", (req, res) =>{
+  //link my html to the get request
 
-app.use(morgan('dev'));
-app.use("/:productId/checkout",express.static(path.join(__dirname, 'lib')));
-
+})
+// app.get("http://localhost:4000/:productID/bundle.js", (req, res) => {
+  
+//   res.send()
+// })
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server listening on port: http://localhost:${PORT}`);
 });
