3039bd6d411d847fda245b3452dd3741b81e25a3
Ubuntu <ubuntu@ip-172-31-21-73.us-east-2.compute.internal>
2019-10-27 00:22:56 +0000

    working proxy on ec2

--- proxyServer/public/server.js
+++ proxyServer/public/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const morgan = require('morgan')
-const PORT = 4001;
+const PORT = 4003;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -12,5 +12,5 @@ app.use(morgan('dev'))
 app.use("/:productID",express.static(path.join(__dirname, 'lib')));
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port: http://localhost:${PORT}`);
+  console.log(`Server listening on port: http://ec2-18-224-110-64.us-east-2.compute.amazonaws.com:${PORT}`);
 });
